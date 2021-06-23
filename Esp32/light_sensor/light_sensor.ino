cdf841ed99829bf34f816be6eeb8da048e2f27a7
hungkhanh <hungpk1412@gmail.com>
2021-06-23 09:02:25 +0700

    add brightness level

--- Esp32/light_sensor/light_sensor.ino
+++ Esp32/light_sensor/light_sensor.ino
@@ -4,6 +4,17 @@
   normal      : 800 - 900
 */
 
+ if (analogValue < 10) {
+   Serial.println(" - Dark");
+ } else if (analogValue < 200) {
+   Serial.println(" - Dim");
+ } else if (analogValue < 500) {
+   Serial.println(" - Light");
+ } else if (analogValue < 800) {
+   Serial.println(" - Bright");
+ } else {
+   Serial.println(" - Very bright");
+ }
 void setup() {
   Serial.begin(9600);
   
@@ -13,5 +24,19 @@ void loop() {
   int analogLight = analogRead(2);
   Serial.print("Sensor light: ");
   Serial.println(analogLight);
+
+
+  if (analogValue > 3000) {
+    Serial.println(" - Dark");
+  } else if (analogValue > 2000) {
+    Serial.println(" - Dim");
+  } else if (analogValue > 700) {
+    Serial.println(" - Light");
+  } else if (analogValue > 300) {
+    Serial.println(" - Bright");
+  } else {
+    Serial.println(" - Very bright");
+  }
+
   delay(1000);
 }
